f88234ca2fdf7394311e52bd817d150472216b5d
Tomas Madajevas <tomas.madajevas@viavisolutions.com>
2016-09-09 11:01:16 +0300

    powershell seach script added

---
+++ find_string_in_file_diffs.ps1
@@ -0,0 +1,30 @@
+﻿Param(
+  [string]$file,
+  [string]$search
+)
+
+$log = hg log $file | select
+
+$log = [System.String]::Join("`n", $log)
+
+$changes = $log -split "`n`n"
+[array]::Reverse($changes);
+
+$pattern = "changeset:\s*(?<revision>\d+)(.|\n)*summary:\s*(?<message>.*)"
+
+$lastRevision = 0
+foreach ($change in $changes)
+{
+    if ($change -match $pattern)
+    {
+        $diff = hg diff -r $lastRevision -r $matches["revision"] $file
+        $lastRevision = $matches["revision"]
+
+        $joined = [String]::Join("`n", $diff)
+
+        if ($joined -like "*$search*")
+        {
+            Write-Host "Change was made in revision $lastRevision"
+        }
+    }
+}
